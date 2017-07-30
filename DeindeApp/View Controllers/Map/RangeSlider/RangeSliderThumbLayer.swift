a63f79f4ec0d0fd6c7dc38ab481d7fa7cab4490a
Juliya Linchevskaya <julia.linch1@gmail.com>
2017-07-30 12:40:39 +0300

    Added slider, slider markers and labels

---
+++ DeindeApp/View Controllers/Map/RangeSlider/RangeSliderThumbLayer.swift
@@ -0,0 +1,35 @@
+//
+//  RangeSliderThumbLayer.swift
+//  RangeSliderExample
+//
+//  Created by Juliya on 27.07.17.
+//  Copyright © 2017 Juliya. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class RangeSliderThumbLayer: CALayer {
+    var highlighted: Bool = false {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    weak var rangeSlider: RangeSlider?
+    
+    
+    override func draw(in ctx: CGContext) {
+        if let slider = rangeSlider {
+            let thumbFrame = bounds.insetBy(dx: 2.0, dy: 2.0)
+            let cornerRadius = thumbFrame.height * slider.curvaceousness / 2.0
+            let thumbPath = UIBezierPath(roundedRect: thumbFrame, cornerRadius: cornerRadius)
+            
+            ctx.setFillColor(slider.thumbTintColor.cgColor)
+            ctx.addPath(thumbPath.cgPath)
+            ctx.fillPath()
+            
+            
+        }
+    }
+    
+}
